53629e7f896578d1653aa2d4416d1a36f345a948
jbisits <jbisits@gmail.com>
2025-02-06 20:29:19 +1100

    Remove pubmed

--- _cite/cite.py
+++ _cite/cite.py
@@ -27,7 +27,7 @@ log("Compiling sources")
 sources = []
 
 # in-order list of plugins to run
-plugins = ["google-scholar", "pubmed", "orcid", "sources"]
+plugins = ["google-scholar", "orcid", "sources"]
 
 # loop through plugins
 for plugin in plugins:
